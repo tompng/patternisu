68d1f263e5083750c498dc9c2a2bf9f3df73a77f
tompng <tomoyapenguin@gmail.com>
2018-09-02 21:59:26 +0900

    dbcache

---
+++ dbcache.rb
@@ -0,0 +1,29 @@
+require 'mysql2-cs-bind'
+
+# only insert pattern
+def load_new_comments
+  return unless @last_comment_id < redis.get('last_comment_id').to_i # optional
+  new_comments = db.xquery('select * from comments where id > ?', @last_comment_id).to_a
+  new_comments.each do |comment|
+    @comments_by_id[comment[:id]] = @comments_by_user[comment[:user_id]] = comment
+  end
+  @last_comment_id = new_comments.last[:id]
+end
+Thread.new { loop { load_new_comments; sleep 1 } } # optional
+
+# changes table pattern
+comment.update params
+db.xquery 'update comments set body = ? where id = ?', comment_body, comment_id
+db.xquery(
+  'insert into changed_records (model_name, model_id, mode) values (?, ?, ?)',
+  :comments, comment_id, :updated
+)
+redis.set 'last_changes_id', db.last_id
+
+# broadcast pattern
+db.xquery 'update comments set body = ? where id = ?', comment_body, comment_id
+redis.publish 'comments_changed', Oj.dump(id: comment_id, body: comment_body)
+
+# redis aggregate pattern
+redis.set "last_comment_id_#{worker_id}", last_comment_id
+last_comment_id = redis.mget(worker_ids.map { |id| "last_comment_id_#{id}" }).map(&:to_i).max
