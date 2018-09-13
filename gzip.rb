709db2f3fc102165df9d660dc8d4773c4403fdc4
tompng <tomoyapenguin@gmail.com>
2018-09-13 18:29:54 +0900

    etag, last_modified

--- gzip.rb
+++ gzip.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'zlib'
 require 'pry'
 get '/' do
+  last_modified = env['HTTP_IF_MODIFIED_SINCE']
+  etag = env['HTTP_IF_NONE_MATCH']
+  last_modified = Time.parse last_modified if last_modified
+  return 304 if last_modified || etag
   tags = Array.new 1024 do
     tag = %w[div b span p h1 h2 h3 h4 h5 i].sample
     "<#{tag}>#{methods.sample}</#{tag}>"
@@ -9,6 +13,8 @@ get '/' do
   html = tags.join "\n"
   return html unless env['HTTP_ACCEPT_ENCODING']&.include? 'gzip'
   headers['Content-Encoding'] = :gzip
+  last_modified Time.at Time.now.to_i
+  etag Time.now.to_i.to_s
   compressed = Zlib.gzip html
   puts "#{html.size}->#{compressed.size} #{compressed.size.fdiv html.size}"
   compressed # must cache it!
